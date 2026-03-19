7c1275aafafd19e81d8927862916c379c6a78f0b
Pedro Jaime <pejotadev07@outlook.com>
2026-03-19 14:32:26 -0300

    Add PUT route for updating videos

--- src/routes/videos.js
+++ src/routes/videos.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/videosControllers')
 router.post('/', controller.postVideos)
 router.get('/', controller.getVideos)
 router.get('/curso/:cursoId', controller.getVideosByCurso)
+router.put('/videos/:id', controller.updateVideo);
 
 module.exports = router
