0c2909e70c8525c4331f817bbcddaa02ea3d8348
Pedro Jaime <pejotadev07@outlook.com>
2026-03-19 11:22:18 -0300

    Add route to get curso by ID

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/cursosControllers')
 
 router.post('/', controller.postCursos)
 router.get('/', controller.getCursos)
+router.get('/cursos/:id', constroller.getCursoById);
 router.delete('/:id', controller.deleteCursos)
 
 module.exports = router
