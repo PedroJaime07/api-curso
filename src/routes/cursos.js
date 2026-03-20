212c156eaed36a3c90fc4d3fcdb1065ff07ccb6e
Pedro Jaime <pejotadev07@outlook.com>
2026-03-20 11:48:51 -0300

    Add PUT route to update curso by ID

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/cursosControllers')
 router.post('/', controller.postCursos)
 router.get('/', controller.getCursos)
 router.get('/:id', controller.getCursoById);
+router.put('/:id', cursosController.updateCurso);
 router.delete('/:id', controller.deleteCursos)
 
 module.exports = router
