bc07bd7a8be3bb5e355b08bf0529fc847d2e9cf5
Pedro Jaime <pejotadev07@outlook.com>
2026-03-20 11:56:48 -0300

    Update cursos.js

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -5,7 +5,7 @@ const controller = require('../controllers/cursosControllers')
 router.post('/', controller.postCursos)
 router.get('/', controller.getCursos)
 router.get('/:id', controller.getCursoById);
-router.put('/:id', cursosController.updateCurso);
+router.put('/:id', controller.updateCurso);
 router.delete('/:id', controller.deleteCursos)
 
 module.exports = router
