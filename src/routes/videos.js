dddc48ec13a5d2047ee6583e2de128ddb81ccdef
Pedro Jaime <pejotadev07@outlook.com>
2026-03-20 10:31:24 -0300

    Add route to get video by ID

--- src/routes/videos.js
+++ src/routes/videos.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/videosControllers')
 router.post('/', controller.postVideos)
 router.get('/', controller.getVideos)
 router.get('/curso/:cursoId', controller.getVideosByCurso)
+router.get("/videos/:id", controller.getVideoById);
 router.put('/videos/:id', controller.updateVideo);
 
 module.exports = router
